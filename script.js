b81569dd50a677f6119373dadfddf105b474ba9d
Hanstephie <hanstephie7@gmail.com>
2026-03-12 23:52:03 +0300

    Update age prompt logic in tap function

--- script.js
+++ script.js
@@ -54,8 +54,8 @@ myButton.addEventListener('click', function() {
 })
 
 function tap() {
-  let age = parseInt(prompt("How old are you now?"))
-
+  prompt("How old are you now?")
+  let age = 17;
   
   if (age === 17) {
     alert("Nine more years remaining for us to meet hopefully🤗")
